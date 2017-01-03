1602fe1ba754f498c22fc21d3d76a2d89549ca6a
Léo Riazan <leo.riazan@hotmail.fr>
2017-01-03 15:46:32 +0100

    test api visualair

--- src/app/API/api.class.ts
+++ src/app/API/api.class.ts
@@ -5,6 +5,9 @@ export class Api
     public type: string;
     public server: string;
     public api: string;
+    public lat: string;
+    public long: string;
+    public key: string;
     public serverWithApiUrl: string;
 
     constructor() { }
