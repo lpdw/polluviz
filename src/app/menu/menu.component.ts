6d710a6ed46633927ae42343c46a523d68dd0b12
Mamihery Rzf <mamihery10@gmail.com>
2017-01-05 15:13:25 +0100

    chec data api

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,6 @@
+//From Angular
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
-  onPage: boolean = false;
-  constructor() { }
+  private _onPage: boolean = false;
+
+  constructor(private _route: ActivatedRoute) { }
 
   ngOnInit() {
+    console.log(this._route);
+  }
+
+  showButtonNavBack(): boolean {
+    return this._onPage;
   }
 
 }
