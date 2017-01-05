b9badc5f1454e95fd29d6a808d68beb7a3a948aa
ValentinKajdan <valentin.kajdan@orange.fr>
2017-01-05 14:26:47 +0100

    menu back + icons

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
-
+  onPage: boolean = false;
   constructor() { }
 
   ngOnInit() {
