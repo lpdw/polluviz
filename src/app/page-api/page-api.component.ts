d832e184489e9d26d3b0d2fc18b658c59fe6cabf
yann <kerfendalyann@gmail.com>
2017-01-05 14:12:35 +0100

    color map

--- src/app/page-api/page-api.component.ts
+++ src/app/page-api/page-api.component.ts
@@ -1,11 +1,13 @@
+import { StyleCompiler } from '@angular/compiler';
 import { flatten } from '@angular/router/src/utils/collection';
 //From angular
-import { Component, OnInit , OnDestroy} from '@angular/core';
+import { Component, OnDestroy, OnInit, style } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 //From our project ,
 // We only import what we need
 import { Api } from '../API/api.class';
+import { StylesMap } from '../API/styles.api';
 import { ApiService } from '../api.service';
 import { AirPollution } from '../API/airpollution.api';
 import { GMAP } from '../API/gmap.api';
@@ -24,8 +26,9 @@ export class PageAPIComponent implements OnInit, OnDestroy {
   private _data: any = null;
   private _myStyleMap: any = [];
   private _gmap: GMAP;
+  private _styleMap: StylesMap;
 
-  constructor(private _route: ActivatedRoute,private _apiService: ApiService) { }
+  constructor(private _route: ActivatedRoute,private _apiService: ApiService) {this._styleMap = new StylesMap(); }
 
   ngOnInit()
   {
@@ -34,7 +37,7 @@ export class PageAPIComponent implements OnInit, OnDestroy {
     this._sub = this._route.params.subscribe(params =>
     {
       //get all params that we need
-      this._options = { websiteName: params['websiteName'], showMap: (params['showMap'] === 'true'), lng: +params['lng'], lat: +params['lat']  };
+      this._options = { websiteName: params['websiteName'], showMap: (params['showMap'] === 'true'), lng: +params['lng'], lat: +params['lat'], typePollution: params['typePollution'] };
 
        //call the ApiService to fectch all data
        this._apiService.getData(params['websiteName'],this._options).toPromise().then(data => { this._data = data });
@@ -69,4 +72,12 @@ export class PageAPIComponent implements OnInit, OnDestroy {
    this._gmap.lat = this._options.lat;
    this._gmap.lng = this._options.lng;
   }
+
+  getStyle(style: string){
+    let styleType;
+    if(this._options.typePollution == style)
+      styleType = this._styleMap.getStyle(style);
+    return styleType;
+  }
+
 }
