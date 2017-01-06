4d6c5149fa949b335884abd9437d61ea6fb145da
pierrecl <pc.clochard@gmail.com>
2017-01-06 15:41:55 +0100

    Fix Faster load about Geolocation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,12 +3,14 @@ import { flatten } from '@angular/router/src/utils/collection';
 //From angular
 import { Component, OnInit, Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { EmitterService } from '../ng2-location/browser-location'
+
 //From our project
 import { Api } from '../API/api.class';
 import { ApiService } from '../api.service';
 import { AirPollution } from '../API/airpollution.api';
 import { ChimicalPollution } from '../API/chimicalpollution.api';
+//Geolocation component
+import { EmitterService } from '../ng2-location/browser-location'
 import { nglocationService } from '../ng2-location/browser-location-service';
 import {EventEmitter} from '@angular/core';
 import {Location} from '../ng2-location/location-interface';
@@ -27,9 +29,12 @@ export class HomeComponent implements OnInit {
   public errorMessage: string;
   private _listApi: Array<Api> = [];
   private _options: any = {};
-  public location: any = {};
+
   private _optionsLocation: any = {};
   public selectedCity: string;
+  public dataLocation: any = {};
+  private lat: number;
+  private lng: number;
 
   constructor(private _apiService: ApiService, private _router: Router, private _ngLocation: nglocationService) {
 
@@ -46,19 +51,16 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.selectedCity = localStorage.getItem('city');
-
+    this.dataLocation = JSON.parse(localStorage['location']);
+    console.log(this.dataLocation.latitude);
+    this.lat = this.dataLocation.latitude;
+    this.lng = this.dataLocation.longitude;
     // Set the options for the geolocation
     this._optionsLocation = {
       enableHighAccuracy: true, //Gets a more accurate position via GPS
       timeout: 5000, //Duration before return to error function
       maximumAge: 0  // Duration of the caching of the current position,  if maximumAge: 0 then the position will never come from the cache, it will always be renewed
     }
-
-    //Check if the browser support the Geolocation
-    if (navigator.geolocation)
-      navigator.geolocation.getCurrentPosition(this.successPosition.bind(this), this.errorPosition, this._optionsLocation);
-    else
-      alert("Le navigateur ne supporte pas la géolocalisation");
 
 
     //TOUS LES API QUI SERONT CHARGE DE BASE
@@ -87,39 +89,15 @@ export class HomeComponent implements OnInit {
     // this._apiService.getData(airvisual.websiteName).toPromise().then(data => console.log(data));
   }
 
-  showPageApi(websiteName: string, lat: string, lng: string, typePollution: string): void {
+  showPageApi(websiteName: string, typePollution: string): void {
     let options = {};
-    this._router.navigate(['/pageApi', { websiteName: websiteName, lat: lat, lng: lng, showMap: true, typePollution: typePollution }]);
+    this._router.navigate(['/pageApi', { websiteName: websiteName, lat: this.lat, lng: this.lng, showMap: true, typePollution: typePollution }]);
   }
 
   redirectToExternalLink(link: string) {
     window.open(link.toLowerCase(), '_blank');
   }
 
-  // Callback succes location
-  successPosition(pos) {
-    this.location = pos.coords;
-  }
 
-  //callback error location
-  errorPosition(error): void {
-    let info = "Erreur lors de la géolocalisation : ";
-    switch (error.code) {
-      case error.TIMEOUT:
-        info += "Timeout !";
-        break;
-      case error.PERMISSION_DENIED:
-        info += "Vous n’avez pas donné la permission";
-        break;
-      case error.POSITION_UNAVAILABLE:
-        info += "La position n’a pu être déterminée";
-        break;
-      case error.UNKNOWN_ERROR:
-        info += "Erreur inconnue";
-        break;
-    }
-    // console.warn(`ERROR(${error.code}): ${error.message}`);
-    console.log(info);
-  }
 
 }
