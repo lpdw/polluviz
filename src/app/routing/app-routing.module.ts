635cdff4c1f5df01f4bd7cc1800ce7ad6f56fa1c
Léo Riazan <leo.riazan@hotmail.fr>
2017-01-13 11:37:34 +0100

    Routing sitemap

--- src/app/routing/app-routing.module.ts
+++ src/app/routing/app-routing.module.ts
@@ -7,12 +7,14 @@ import { MenuComponent } from '../menu/menu.component';
 import { HomeComponent } from '../home/home.component';
 import { PageAPIComponent } from '../page-api/page-api.component';
 import { TermsComponent } from '../terms/terms.component';
+import { SitemapComponent } from '../sitemap/sitemap.component';
 import { PageNotFoundComponent } from '../page-not-found/page-not-found.component';
 
 const appRoutes : Routes = [
   { path: '', component: HomeComponent, },
   { path: 'pageApi', component: PageAPIComponent},
   { path : 'terms', component: TermsComponent},
+  { path : 'sitemap', component: SitemapComponent},  
   { path: '**', component: PageNotFoundComponent }
 ];
 
